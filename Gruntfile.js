5c34d3c8a2d2ed691e7e7a09ce2b07198f065b7e
jorgejar <jorge.jaramillo@docker.com>
2017-06-18 12:54:48 -0700

    minify process
    Signed-off-by: jorgejar <jorge.jaramillo@docker.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,9 +4,24 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-webpack');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
 
+  require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
+  const initMinify = 'node ./node_modules/google-closure-compiler-js/cmd.js';
+  const minifyCommand = [
+    initMinify + ' ./bin/js/popup.js > ./bin/js/popup.min.js;',
+    initMinify + ' ./bin/js/content-script.js > ./bin/js/content-script.min.js;',
+    initMinify + ' ./bin/js/options.js > ./bin/js/options.min.js;',
+    'mv ./bin/js/popup.min.js ./bin/js/popup.js;',
+    'mv ./bin/js/content-script.min.js ./bin/js/content-script.js;',
+    'mv ./bin/js/options.min.js ./bin/js/options.js;',
+
+  ].join(' ');
   let gconfig = {
+    shell: {
+      minify: {
+        command: minifyCommand
+      }
+    },
     clean: {
       main: ['bin']
     },
@@ -125,6 +140,6 @@ module.exports = function (grunt) {
     }
   };
   grunt.initConfig(gconfig);
-  grunt.registerTask('default', ['clean', 'copy', 'webpack']);
+  grunt.registerTask('default', ['clean', 'copy', 'webpack', 'shell:minify']);
   grunt.registerTask('dev', ['default', 'watch']);
 };
